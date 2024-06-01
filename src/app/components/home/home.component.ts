c44ca46f0bac641fdf140ec1a9637c676e628153
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-06-01 15:13:26 +0500

    *added to home: card-popular

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  // @Input() books: any[] = [];
+  @Input() books: any[] = [
+    {
+      image: 'assets/images/image1.png',
+      title: 'Название книги 1',
+      author: 'Автор 1'
+    },
+    {
+      image: 'assets/images/image2.png',
+      title: 'Название книги 2',
+      author: 'Автор 2'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    {
+      image: 'assets/images/image3.png',
+      title: 'Название книги 3',
+      author: 'Автор 3'
+    },
+    // Добавьте другие книги по аналогии
+  ];
 }
--- src/app/components/pages/popular/popular.component.ts
+++ src/app/components/pages/popular/popular.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-popular',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './popular.component.scss'
 })
 export class PopularComponent {
-
+  @Input() popularBooks: any[] = [];
 }
